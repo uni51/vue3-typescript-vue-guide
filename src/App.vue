f4860a77a2c90e349433d2f92a4a7fe6d9d1b83e
Koichi.Shimomura <unieye51@gmail.com>
2022-07-11 13:57:36 +0900

    5-31 静的なリスト

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+// import HelloWorld from './components/HelloWorld.vue'
+import Tweet from './components/Tweet.vue'
+</script>
+
+<template>
+  <Tweet />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Tweet.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const tweets = [{id: '0', description: 'Hello, world!'}, {id: '1', description: 'this is the second tweet'}]
+
+</script>
+
+<template>
+  <div class="container">
+    <h1>Tweeter</h1>
+    <div class="form-container">
+      <input />
+      <button class="save-button">post</button>
+    </div>
+    <div class="tweet-container">
+      <ul>
+        <li v-for="tweet in tweets" :key="tweet.id" class="tweet-list">
+          <span>{{ tweet.description }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.form-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aliceblue;
+  padding: 24px 0;
+  width: 60%;
+  margin-bottom: 12px;
+  border-radius: 4px;
+}
+
+.tweet-list {
+  list-style: none;
+}
+
+button {
+  color: #fff;
+  font-weight: bold;
+  background-color: #68c9c9;
+  border-radius: 2px;
+  border: none;
+  padding: 5px;
+  margin-top: 10px;
+}
+</style>
